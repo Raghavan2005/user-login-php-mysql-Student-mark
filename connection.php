193d84d3011b9bcce0629789675722a8af9c42e2
Raghavan <raghavan.work@outlook.com>
2023-03-26 21:26:39 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     $host = "localhost";  
     $user = "root";  
     $password = '';  
-    $db_name = "raghavan";  
+    $db_name = "your_databasename";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
